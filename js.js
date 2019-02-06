e0c2b5923c3cecb13b750b42a86b5c2065737303
Swartz Krisztián <webevolution13@gmail.com>
2019-02-06 18:10:15 +0100

    header 90

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
 
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
